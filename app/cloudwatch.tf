6442ea10051f1c567402b172b0101e2a0e9c5cba
milh0use <steven.hirschorn@gmail.com>
2021-04-21 12:56:07 +0100

    Add cloudwatch monitoring for Wordpress

---
+++ app/cloudwatch.tf
@@ -0,0 +1,156 @@
+# TODO Resources to monitor
+
+# ASG
+# EFS
+# LB
+
+resource "aws_cloudwatch_dashboard" "discovery" {
+  dashboard_name = "discovery-dashboard"
+
+  dashboard_body = <<EOF
+{
+    "widgets": [
+        {
+            "type": "text",
+            "x": 0,
+            "y": 0,
+            "width": 24,
+            "height": 1,
+            "properties": {
+                "markdown": "\n# Wordpress Servers\n"
+            }
+        },
+        {
+            "type": "text",
+            "x": 0,
+            "y": 1,
+            "width": 24,
+            "height": 1,
+            "properties": {
+                "markdown": "\n## Web Activity\n"
+            }
+        },
+        {
+            "type": "metric",
+            "x": 0,
+            "y": 2,
+            "width": 12,
+            "height": 6,
+            "properties": {
+                "view": "timeSeries",
+                "stacked": false,
+                "metrics": [
+                    [ "AWS/ApplicationELB", "ActiveConnectionCount", "LoadBalancer", aws_lb.website_public.arn_suffix ],
+                    [ ".", "RequestCount", ".", "." ]
+                ],
+                "region": "eu-west-2",
+                "title": "Connections and Requests"
+            }
+        },
+        {
+            "type": "metric",
+            "x": 12,
+            "y": 2,
+            "width": 12,
+            "height": 6,
+            "properties": {
+                "view": "timeSeries",
+                "stacked": false,
+                "metrics": [
+                    [ "AWS/ApplicationELB", "HTTPCode_Target_5XX_Count", "LoadBalancer", aws_lb.website_public.arn_suffix ],
+                    [ ".", "HTTPCode_Target_2XX_Count", ".", "." ]
+                ],
+                "region": "eu-west-2",
+                "title": "HTTP Status Codes",
+                "period": 300
+            }
+        },
+        {
+            "type": "text",
+            "x": 0,
+            "y": 8,
+            "width": 24,
+            "height": 1,
+            "properties": {
+                "markdown": "\n## Disk Activity\n"
+            }
+        },
+        {
+            "type": "metric",
+            "x": 0,
+            "y": 9,
+            "width": 12,
+            "height": 6,
+            "properties": {
+                "view": "timeSeries",
+                "stacked": false,
+                "metrics": [
+                    [ "AWS/EFS", "StorageBytes", "StorageClass", "Total", "FileSystemId", "fs-625d7f93" ]
+                ],
+                "region": "eu-west-2",
+                "title": "EFS Storage"
+            }
+        },
+        {
+            "type": "metric",
+            "x": 12,
+            "y": 9,
+            "width": 12,
+            "height": 6,
+            "properties": {
+                "view": "timeSeries",
+                "stacked": false,
+                "metrics": [
+                    [ "AWS/EFS", "TotalIOBytes", "FileSystemId", aws_efs_file_system.website.id ]
+                ],
+                "region": "eu-west-2",
+                "title": "EFS Total IO"
+            }
+        },
+        {
+            "type": "text",
+            "x": 0,
+            "y": 15,
+            "width": 24,
+            "height": 1,
+            "properties": {
+                "markdown": "\n## Server Stats\n"
+            }
+        },
+        {
+            "type": "metric",
+            "x": 0,
+            "y": 16,
+            "width": 12,
+            "height": 6,
+            "properties": {
+                "view": "timeSeries",
+                "stacked": false,
+                "metrics": [
+                    [ "AWS/EC2", "CPUUtilization" ]
+                ],
+                "region": "eu-west-2",
+                "title": "EC2 CPU"
+            }
+        },
+        {
+            "type": "metric",
+            "x": 12,
+            "y": 16,
+            "width": 12,
+            "height": 6,
+            "properties": {
+                "view": "timeSeries",
+                "stacked": false,
+                "metrics": [
+                    [ "AWS/EC2", "NetworkIn" ],
+                    [ ".", "NetworkOut" ]
+                ],
+                "region": "eu-west-2",
+                "title": "EC2 Network"
+            }
+        }
+    ]
+}
+EOF
+}
