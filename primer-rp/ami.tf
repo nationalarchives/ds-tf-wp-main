9c7e9052bb80b0acaf1cada7a567dced8ed2f052
Karl Kern <kkern@nationalarchives.gov.uk>
2021-06-07 17:13:02 +0100

    remove local-exec

--- primer-rp/ami.tf
+++ primer-rp/ami.tf
@@ -6,14 +6,3 @@ resource "aws_ami_from_instance" "rp_ami" {
         Name = var.ami_name
     })
 }
-
-# clean up
-# 1. remove the ami from state
-# 2. destroy instance
-resource "null_resource" "run_terraform" {
-    depends_on = [aws_ami_from_instance.rp_ami]
-
-    provisioner "local-exec" {
-        command = "terraform state rm aws_ami_from_instance.rp_ami && terraform destroy aws_instance.rp_primer"
-    }
-}
