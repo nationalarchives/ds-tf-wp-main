a6969f8d663f7221457da4bee1b07f127480883d
Karl Kern <kkern@nationalarchives.gov.uk>
2021-06-07 14:48:13 +0100

    add iam elements for instance

---
+++ primer-rp/iam.tf
@@ -0,0 +1,39 @@
+# -----------------------------------------------------------------------------
+# IAM instance role and instance profile
+# -----------------------------------------------------------------------------
+resource "aws_iam_role" "primer_rp" {
+    name               = var.instance_role
+    assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+               "Service": "ec2.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": "RPAssumeRole"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "rp_deployment_s3" {
+    name        = var.instance_role_policy_name
+    description = "RP deployment S3 access"
+
+    policy = var.instance_role_policy
+}
+
+resource "aws_iam_role_policy_attachment" "wp_deployment_s3_policy" {
+    policy_arn = aws_iam_policy.rp_deployment_s3.arn
+    role       = aws_iam_role.primer_rp.id
+}
+
+resource "aws_iam_instance_profile" "primer" {
+    name = var.instance_profile
+    path = "/"
+    role = aws_iam_role.primer_rp.name
+}
---
+++ primer-rp/vars.tf
@@ -0,0 +1,4 @@
+variable "instance_role" {}
+variable "instance_profile" {}
+variable "instance_role_policy_name" {}
+variable "instance_role_policy" {}
