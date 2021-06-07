2bcbcb0a20e4bf99ebae2060a6f54293e46e25e8
Karl Kern <kkern@nationalarchives.gov.uk>
2021-06-07 16:40:02 +0100

    correct ami name

--- primer-rp/ami.tf
+++ primer-rp/ami.tf
@@ -1,10 +1,6 @@
-locals {
-    ts = formatdate("YYYY-MM-DD hh-mm-ss", timestamp())
-}
-
 resource "aws_ami_from_instance" "rp_ami" {
-    name               = "${local.environment}-${local.service} ${local.ts}"
-    source_instance_id = aws_instance.wp_primer
+    name               = var.ami_name
+    source_instance_id = aws_instance.rp_primer.id
 
     tags = merge(var.tags, {
         Name = var.ami_name
