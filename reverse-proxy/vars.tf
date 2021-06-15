4ba857e79f7fcb89de967280fc800a832201d78f
Karl Kern <kkern@nationalarchives.gov.uk>
2021-06-15 23:46:46 +0100

    add cidrs for vpn and on-prem

--- reverse-proxy/vars.tf
+++ reverse-proxy/vars.tf
@@ -1,31 +1,21 @@
 variable "environment" {}
-
 variable "service" {}
-
 variable "cost_centre" {}
-
 variable "owner" {}
-
 variable "created_by" {}
 
 variable "vpc_id" {}
 
 variable "public_subnet_a_id" {}
-
 variable "public_subnet_b_id" {}
-
 variable "private_subnet_a_id" {}
-
 variable "private_subnet_b_id" {}
 
 variable "asg_max_size" {}
-
 variable "asg_min_size" {}
-
 variable "asg_desired_capacity" {}
 
 variable "asg_health_check_grace_period" {}
-
 variable "asg_health_check_type" {}
 
 variable "patch_group_name" {}
@@ -52,6 +42,8 @@ variable "editorial_domain_name" {}
 variable "public_domain_name" {}
 
 variable "everyone" {}
+variable "vpn_cidr" {}
+variable "on_prem_cidrs" {}
 
 variable "efs_backup_schedule" {}
 
