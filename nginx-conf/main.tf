28f2885f8020b45de606c3ba27e278c976d01238
Karl Kern <kkern@nationalarchives.gov.uk>
2021-06-17 16:06:29 +0100

    move scripts to conf

--- nginx-conf/main.tf
+++ nginx-conf/main.tf
@@ -1,34 +1,49 @@
 resource "aws_s3_bucket_object" "nginx_conf" {
-    bucket  = var.deployment_s3_bucket
-    key     = "${var.service}/${var.nginx_conf_s3_key}/nginx.conf"
-    source  = "scripts/nginx.conf"
-    etag    = filemd5("scripts/nginx.conf")
+    bucket = var.deployment_s3_bucket
+    key    = "${var.service}/${var.nginx_conf_s3_key}/nginx.conf"
+    source = templatefile("${path.module}/scripts/nginx.conf", {
+        environment      = var.environment,
+        set_real_ip_from = var.set_real_ip_from,
+        resolver         = var.resolver,
+        ups_website      = var.ups_website,
+        ups_appslb       = var.ups_appslb,
+        ups_legacy_apps  = var.ups_legacy_apps
+    })
+    etag   = filemd5("${path.module}/scripts/nginx.conf")
 }
 
 resource "aws_s3_bucket_object" "admin_conf" {
-    bucket  = var.deployment_s3_bucket
-    key     = "${var.service}/${var.nginx_conf_s3_key}/wp_admin.conf"
-    source  = "scripts/wp_admin.conf"
-    etag    = filemd5("scripts/wp_admin.conf")
+    bucket = var.deployment_s3_bucket
+    key    = "${var.service}/${var.nginx_conf_s3_key}/wp_admin.conf"
+    source = templatefile("${path.module}/scripts/wp_admin.conf", {
+        environment      = var.environment,
+        set_real_ip_from = var.set_real_ip_from,
+        resolver         = var.resolver
+    })
+    etag   = filemd5("${path.module}/scripts/wp_admin.conf")
 }
 
 resource "aws_s3_bucket_object" "admin_subdomain_conf" {
-    bucket  = var.deployment_s3_bucket
-    key     = "${var.service}/${var.nginx_conf_s3_key}/wp_admin_subdomain.conf"
-    source  = "scripts/wp_admin_subdomain.conf"
-    etag    = filemd5("scripts/wp_admin_subdomain.conf")
+    bucket = var.deployment_s3_bucket
+    key    = "${var.service}/${var.nginx_conf_s3_key}/wp_admin_subdomain.conf"
+    source = templatefile("${path.module}/scripts/wp_admin_subdomain.conf", {
+        environment      = var.environment,
+        set_real_ip_from = var.set_real_ip_from,
+        resolver         = var.resolver
+    })
+    etag   = filemd5("${path.module}/scripts/wp_admin_subdomain.conf")
 }
 
 resource "aws_s3_bucket_object" "admin_ips_conf" {
-    bucket  = var.deployment_s3_bucket
-    key     = "${var.service}/${var.nginx_conf_s3_key}/admin_ips.conf"
-    source  = "scripts/admin_ips.conf"
-    etag    = filemd5("scripts/admin_ips.conf")
+    bucket = var.deployment_s3_bucket
+    key    = "${var.service}/${var.nginx_conf_s3_key}/admin_ips.conf"
+    source = "${path.module}/scripts/admin_ips.conf"
+    etag   = filemd5("${path.module}/scripts/admin_ips.conf")
 }
 
 resource "aws_s3_bucket_object" "nginx_logrotate" {
-    bucket  = var.deployment_s3_bucket
-    key     = "${var.service}/${var.nginx_conf_s3_key}/nginx"
-    source  = "scripts/nginx"
-    etag    = filemd5("scripts/nginx")
+    bucket = var.deployment_s3_bucket
+    key    = "${var.service}/${var.nginx_conf_s3_key}/nginx"
+    source = "${path.module}/scripts/nginx"
+    etag   = filemd5("${path.module}/scripts/nginx")
 }
--- nginx-conf/vars.tf
+++ nginx-conf/vars.tf
@@ -1,5 +1,13 @@
 variable "deployment_s3_bucket" {}
-
 variable "nginx_conf_s3_key" {}
 
 variable "service" {}
+
+# variables for scripts
+variable "environment" {}
+variable "set_real_ip_from" {}
+variable "vpc_cidr" {}
+variable "resolver" {}
+variable "ups_website" {}
+variable "ups_appslb" {}
+variable "ups_legacy_apps" {}
